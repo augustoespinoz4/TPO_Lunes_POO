e50ec7a762f33e95e081885184289b27d4e29606
augustoespinoz4 <129131293+augustoespinoz4@users.noreply.github.com>
2024-05-25 15:52:41 -0300

    Update Usuario.java

--- TPO_Lunes_POO/src/Clases/Usuario.java
+++ TPO_Lunes_POO/src/Clases/Usuario.java
@@ -1,4 +1,36 @@
 package Clases;
 
-public class Usuario {
+public abstract class Usuario {
+    protected String nombreCompleto; // Nombre de usuario de la pagina
+    protected String correoEletronico;
+    protected String contrasena;
+
+    public abstract void AgregarProducto(Producto producto);
+    public  abstract  void EliminarProducto(Producto producto);
+
+    protected  boolean Verificador(String nombreCompleto , String correoEletronico , String contrasena) {
+        boolean SoyYo=false;
+        if (this.nombreCompleto == nombreCompleto && this.correoEletronico == correoEletronico && this.contrasena == contrasena){
+            SoyYo=true;
+        }
+        return SoyYo;
+    }
+
+    public Usuario(String nombreCompleto, String correoEletronico, String contrasena) {
+        this.nombreCompleto= nombreCompleto;
+        this.correoEletronico = correoEletronico;
+        this.contrasena = contrasena;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public String getCorreoEletronico() {
+        return correoEletronico;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
 }
