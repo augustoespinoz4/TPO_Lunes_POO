bdcd5dbf0d97237ba25e604999981f4f28a1c119
NicolasAlfonsin <128539415+NicolasAlfonsin@users.noreply.github.com>
2024-05-25 15:58:59 -0300

    Add files via upload

---
+++ TPO_Lunes_POO/src/Clases/Cliente.java
@@ -0,0 +1,14 @@
+public final class Cliente extends Usuario{
+
+    public void agregraProducto(Producto producto){
+
+    }
+    public  void eleminarProducto(Producto producto){
+
+    }
+
+    public Cliente(String nombreCompleto, String correoEletronico, String contrasena) {
+        super(nombreCompleto, correoEletronico, contrasena);
+    }
+
+}
