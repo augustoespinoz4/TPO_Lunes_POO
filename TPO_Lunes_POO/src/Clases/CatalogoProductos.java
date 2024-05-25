028a1979d62d5616b8ff7a20fafab1ac38e4694f
augustoespinoz4 <129131293+augustoespinoz4@users.noreply.github.com>
2024-05-25 16:24:07 -0300

    Add files via upload

---
+++ TPO_Lunes_POO/src/Clases/CatalogoProductos.java
@@ -0,0 +1,26 @@
+package Clases;
+
+import java.util.ArrayList;
+
+public class CatalogoProductos {
+    private  ArrayList<Producto> ListaProducto;
+
+    public CatalogoProductos( ArrayList<Producto>listaProducto) {
+        this.ListaProducto = listaProducto;
+    }
+    private boolean existeProducto(int codigo){
+        boolean resultado = false;
+
+        return resultado;
+    }
+
+    public void Agregar(Producto producto){
+
+    }
+    public void Modificar(Producto producto){
+
+    }
+    public void Eliminar(Producto producto){
+
+    }
+}
