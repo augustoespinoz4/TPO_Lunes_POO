fbb2586910fda96fb6a09c4b76a9d6b66206acf0
augustoespinoz4 <129131293+augustoespinoz4@users.noreply.github.com>
2024-05-25 15:59:37 -0300

    Update Cliente.java

--- TPO_Lunes_POO/src/Clases/Cliente.java
+++ TPO_Lunes_POO/src/Clases/Cliente.java
@@ -1,14 +1,38 @@
-public final class Cliente extends Usuario{
+package Clases;
 
-    public void agregraProducto(Producto producto){
-
-    }
-    public  void eleminarProducto(Producto producto){
-
-    }
+public final class Cliente extends Usuario {
 
     public Cliente(String nombreCompleto, String correoEletronico, String contrasena) {
         super(nombreCompleto, correoEletronico, contrasena);
     }
 
+    @Override
+    public void AgregarProducto(Producto producto) {
+
+    }
+
+    @Override
+    public void EliminarProducto(Producto producto) {
+
+    }
+
+    @Override
+    protected boolean Verificador(String nombreCompleto, String correoEletronico, String contrasena) {
+        return super.Verificador(nombreCompleto, correoEletronico, contrasena);
+    }
+
+    @Override
+    public String getNombreCompleto() {
+        return super.getNombreCompleto();
+    }
+
+    @Override
+    public String getCorreoEletronico() {
+        return super.getCorreoEletronico();
+    }
+
+    @Override
+    public String getContrasena() {
+        return super.getContrasena();
+    }
 }
