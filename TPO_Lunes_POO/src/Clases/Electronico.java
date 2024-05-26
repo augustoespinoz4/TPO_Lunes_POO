1093e3d38955ed11e7954d4bccb0388ea2bf3eb1
JoaquinUade <164223093+JoaquinUade@users.noreply.github.com>
2024-05-26 16:16:14 -0300

    Update Electronico.java

--- TPO_Lunes_POO/src/Clases/Electronico.java
+++ TPO_Lunes_POO/src/Clases/Electronico.java
@@ -4,8 +4,8 @@ public final class Electronico extends  Producto{
 
     private int garantia;
 
-    public Electronico(int codigo, String nombre, String descricion, float precio, int garantia) {
-        super(codigo, nombre, descricion, precio);
+    public Electronico(int codigo, String nombre, String descripcion, float precio, int garantia) {
+        super(codigo, nombre, descripcion, precio);
         this.garantia = garantia;
     }
 
@@ -18,7 +18,7 @@ public final class Electronico extends  Producto{
         return "Electronico{" +
                 "codigo=" + codigo +
                 ", nombre='" + nombre + '\'' +
-                ", descricion='" + descricion + '\'' +
+                ", descricion='" + descripcion + '\'' +
                 ", precio=" + precio +
                 ", garantia=" + garantia +
                 '}';
