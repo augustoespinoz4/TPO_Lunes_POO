98f1e29fc173857437cc894de4247fde3201350a
RamiroGarciaUade <164058644+RamiroGarciaUade@users.noreply.github.com>
2024-05-27 16:18:07 -0300

    Update Controlador.java
    cambios en clase

--- TPO_Lunes_POO/src/Controlador/Controlador.java
+++ TPO_Lunes_POO/src/Controlador/Controlador.java
@@ -12,17 +12,19 @@ public class Controlador {
     private ArrayList<Administrador> administradores;
     private  CatalogoProductos catalogo;
 
+    private Carrito carrito;
+
     public Controlador(ArrayList<Cliente> clientes, ArrayList<Administrador> administradores, CatalogoProductos catalogo) {
         this.clientes = clientes;
         this.administradores = administradores;
         this.catalogo = catalogo;
     }
-    public boolean realizarLogin(String nombreCompleto , String email , String contrasena){ // de volver true o false para comprobar la existecia del usuario
+    public String realizarLogin(String nombreCompleto , String email , String contrasena){ // de volver true o false para comprobar la existecia del usuario
         int i =0;
-        boolean seEncontro=false;
-        while (i < administradores.size() && !seEncontro){
+        String seEncontro="Ninguno";
+        while (i < administradores.size() && seEncontro != "Administrador"){
             if ( administradores.get(i).getNombreCompleto() == nombreCompleto && administradores.get(i).getCorreoEletronico() == email && administradores.get(i).getContrasena()==contrasena){
-                seEncontro=true;
+                seEncontro="Administrador";
                 // invocar el tipo de usuario con mesaje
             }
             else {
@@ -30,9 +32,10 @@ public class Controlador {
             }
         }
         int j=0;
-        while (j < clientes.size() && !seEncontro){
+        while (j < clientes.size() && seEncontro != "Cliente"){
             if (clientes.get(j).getNombreCompleto() == nombreCompleto && clientes.get(j).getCorreoEletronico() == email && clientes.get(j).getContrasena()==contrasena){
-                seEncontro=true;
+                seEncontro="Cliente";
+                this.carrito = new Carrito(clientes.get(i));
             }
             else {
                 j++;
@@ -41,12 +44,12 @@ public class Controlador {
         return seEncontro;
 
     }
-    public void registrarCliente (String nombre , String email , String contrasena){
-        Cliente cliente= new Cliente(nombre,email,contrasena);
+    public void registrarCliente (String nombreCompleto , String gmail , String contrasena){
+        Cliente cliente= new Cliente(nombreCompleto,gmail,contrasena);
         int j=0;
         boolean seEncontro=false;
         while (j < clientes.size() && !seEncontro){
-            if ( clientes.get(j).getNombreCompleto() == nombre && clientes.get(j).getCorreoEletronico() == email && clientes.get(j).getContrasena()==contrasena){
+            if ( clientes.get(j).getNombreCompleto() == nombreCompleto && clientes.get(j).getCorreoEletronico() == gmail && clientes.get(j).getContrasena()==contrasena){
                 seEncontro=true;
             }
             else {
@@ -71,17 +74,15 @@ public class Controlador {
             }
         }
     }
-        public void agregarProducto(int codigo, String nombre, String descripcion, float precio) {
-        Producto nuevoProducto = new Producto(codigo, nombre, descripcion, precio);
-        try {
-            FileWriter fwPro = new FileWriter("productos.txt", true);
-            PrintWriter pwPro = new PrintWriter(fwPro);
-            pwPro.println(nuevoProducto.getCodigo() + "," + nuevoProducto.getNombre() + "," + nuevoProducto.getDescricion() + "," + nuevoProducto.getPrecio());
-            pwPro.close();
-            fwPro.close();
-        } catch (IOException e){
-            System.err.println("Error al escribir en el archivo.");
-            e.printStackTrace();
+        public void agregarProducto(Producto producto) {
+        carrito.agregarItem(producto);
         }
+        public void eleminarProducto(Producto producto) {
+        carrito.eliminarItem(producto);
+        }
+        public void borrarCarrito(){
+        carrito.borrarCarrito();
+        }
+
 
 }
