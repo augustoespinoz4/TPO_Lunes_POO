e976df27035362e0f45d60bba3d6180aa341dce3
JoaquinUade <164223093+JoaquinUade@users.noreply.github.com>
2024-05-31 21:28:10 -0300

    Update Controlador.java

--- TPO_Lunes_POO/src/Controlador/Controlador.java
+++ TPO_Lunes_POO/src/Controlador/Controlador.java
@@ -1,7 +1,8 @@
 package Controlador;
 
 import Clases.*;
-
+import javax.swing.JOptionPane;
+import javax.swing.*;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -23,7 +24,7 @@ public class Controlador {
         int i =0;
         String seEncontro="Ninguno";
         while (i < administradores.size() && seEncontro != "Administrador"){
-            if ( administradores.get(i).getNombreCompleto() == nombreCompleto && administradores.get(i).getCorreoEletronico() == email && administradores.get(i).getContrasena()==contrasena){
+            if (administradores.get(i).getCorreoEletronico() == email && administradores.get(i).getContrasena()==contrasena){
                 seEncontro="Administrador";
                 // invocar el tipo de usuario con mesaje
             }
@@ -33,9 +34,9 @@ public class Controlador {
         }
         int j=0;
         while (j < clientes.size() && seEncontro != "Cliente"){
-            if (clientes.get(j).getNombreCompleto() == nombreCompleto && clientes.get(j).getCorreoEletronico() == email && clientes.get(j).getContrasena()==contrasena){
+            if (clientes.get(j).getCorreoEletronico() == email && clientes.get(j).getContrasena()==contrasena){
                 seEncontro="Cliente";
-                this.carrito = new Carrito(clientes.get(i));
+                this.carrito = new Carrito(clientes.get(j));
             }
             else {
                 j++;
@@ -45,44 +46,53 @@ public class Controlador {
 
     }
     public void registrarCliente (String nombreCompleto , String gmail , String contrasena){
-        Cliente cliente= new Cliente(nombreCompleto,gmail,contrasena);
-        int j=0;
-        boolean seEncontro=false;
-        while (j < clientes.size() && !seEncontro){
-            if ( clientes.get(j).getNombreCompleto() == nombreCompleto && clientes.get(j).getCorreoEletronico() == gmail && clientes.get(j).getContrasena()==contrasena){
-                seEncontro=true;
+        if (gmail.endsWith("@gmail.com")) {
+            Cliente cliente= new Cliente(nombreCompleto,gmail,contrasena);
+            int j=0;
+            boolean seEncontro=false;
+            while (j < clientes.size() && !seEncontro){
+                if ( clientes.get(j).getCorreoEletronico() == gmail ){
+                    seEncontro=true;
+                }
+                else {
+                    j++;
+                }
             }
-            else {
-                j++;
+            if (!seEncontro){
+                clientes.add(cliente);
+                try {
+                    FileWriter fw = new FileWriter("clientes.txt", true);
+                    PrintWriter pw = new PrintWriter(fw);
+
+                    pw.println(nombreCompleto + "," + gmail + "," + contrasena);
+
+                    pw.close();
+                    fw.close();
+                    System.out.println("Cliente registrado exitosamente.");
+                    JOptionPane.showMessageDialog(null, "Se registro correctamente!!!", "Éxito", JOptionPane.INFORMATION_MESSAGE);
+
+                } catch (IOException e) {
+                    System.err.println("Error al escribir en el archivo.");
+                    e.printStackTrace();
+                }
+
+            }else{
+                JOptionPane.showMessageDialog(null, "Mail ya en uso!!!", "Error", JOptionPane.ERROR_MESSAGE);
             }
-        }
-        if (!seEncontro){
-            clientes.add(cliente);
-            try {
-                FileWriter fw = new FileWriter("clientes.txt", true);
-                PrintWriter pw = new PrintWriter(fw);
 
-                pw.println(nombreCompleto + "," + gmail + "," + contrasena);
-
-                pw.close();
-                fw.close();
-                System.out.println("Cliente registrado exitosamente.");
-
-            } catch (IOException e) {
-                System.err.println("Error al escribir en el archivo.");
-                e.printStackTrace();
-            }
+        }else if (!gmail.endsWith("@gmail.com")){
+            JOptionPane.showMessageDialog(null, "Rellene el campo gmail", "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
-        public void agregarProducto(Producto producto) {
+    public void agregarProducto(Producto producto) {
         carrito.agregarItem(producto);
-        }
-        public void eleminarProducto(Producto producto) {
+    }
+    public void eleminarProducto(Producto producto) {
         carrito.eliminarItem(producto);
-        }
-        public void borrarCarrito(){
+    }
+    public void borrarCarrito(){
         carrito.borrarCarrito();
-        }
+    }
 
 
 }
