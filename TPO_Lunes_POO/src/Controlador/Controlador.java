ed4764ac3266025b2a2037bf49720d149dfcc611
JoaquinUade <164223093+JoaquinUade@users.noreply.github.com>
2024-06-01 00:40:47 -0300

    Update Controlador.java

--- TPO_Lunes_POO/src/Controlador/Controlador.java
+++ TPO_Lunes_POO/src/Controlador/Controlador.java
@@ -1,12 +1,11 @@
 package Controlador;
+import java.io.*;
 
 import Clases.*;
 import javax.swing.JOptionPane;
 import javax.swing.*;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Controlador {
     private ArrayList<Cliente> clientes;
@@ -15,16 +14,16 @@ public class Controlador {
 
     private Carrito carrito;
 
-    public Controlador(ArrayList<Cliente> clientes, ArrayList<Administrador> administradores, CatalogoProductos catalogo) {
-        this.clientes = clientes;
-        this.administradores = administradores;
+    public Controlador() {
+        this.clientes = new ArrayList<>();
+        this.administradores = new ArrayList<>();
         this.catalogo = catalogo;
     }
     public String realizarLogin(String nombreCompleto , String email , String contrasena){ // de volver true o false para comprobar la existecia del usuario
         int i =0;
         String seEncontro="Ninguno";
-        while (i < administradores.size() && seEncontro != "Administrador"){
-            if (administradores.get(i).getCorreoEletronico() == email && administradores.get(i).getContrasena()==contrasena){
+        while (i < administradores.size() && !seEncontro.equals("Administrador")){
+            if (Objects.equals(administradores.get(i).getCorreoEletronico(), email) && Objects.equals(administradores.get(i).getContrasena(), contrasena)){
                 seEncontro="Administrador";
                 // invocar el tipo de usuario con mesaje
             }
@@ -33,8 +32,8 @@ public class Controlador {
             }
         }
         int j=0;
-        while (j < clientes.size() && seEncontro != "Cliente"){
-            if (clientes.get(j).getCorreoEletronico() == email && clientes.get(j).getContrasena()==contrasena){
+        while (j < clientes.size() && !seEncontro.equals("Cliente") && !seEncontro.equals("Administrador")){
+            if (Objects.equals(clientes.get(j).getCorreoEletronico(), email) && Objects.equals(clientes.get(j).getContrasena(), contrasena)){
                 seEncontro="Cliente";
                 this.carrito = new Carrito(clientes.get(j));
             }
@@ -51,7 +50,7 @@ public class Controlador {
             int j=0;
             boolean seEncontro=false;
             while (j < clientes.size() && !seEncontro){
-                if ( clientes.get(j).getCorreoEletronico() == gmail ){
+                if (Objects.equals(clientes.get(j).getCorreoEletronico(), gmail)){
                     seEncontro=true;
                 }
                 else {
@@ -60,20 +59,40 @@ public class Controlador {
             }
             if (!seEncontro){
                 clientes.add(cliente);
+                String nombreArchivo = "clientes.txt"; // Nombre del archivo
+
+                // Ruta de la carpeta fuera de src
+                String rutaCarpeta = "archivos/";
+                // Ruta completa del archivo
+                String rutaArchivo = rutaCarpeta + nombreArchivo;
+
+                // Verificar si la carpeta existe, si no existe, crearla
+                File carpeta = new File(rutaCarpeta);
+                if (!carpeta.exists()) {
+                    carpeta.mkdirs(); // Crea la carpeta y cualquier carpeta padre necesaria
+                }
+
+                // Escribir en el archivo de clientes
+                FileWriter writer = null;
                 try {
-                    FileWriter fw = new FileWriter("clientes.txt", true);
-                    PrintWriter pw = new PrintWriter(fw);
+                    writer = new FileWriter(rutaArchivo, true); // "true" indica que se agregará al final del archivo
+                    PrintWriter pw = new PrintWriter(writer);
 
-                    pw.println(nombreCompleto + "," + gmail + "," + contrasena);
+                    // Escribir los datos del cliente en una línea nueva
+                    pw.println("Cliente," + nombreCompleto + "," + gmail + "," + contrasena);
 
-                    pw.close();
-                    fw.close();
                     System.out.println("Cliente registrado exitosamente.");
-                    JOptionPane.showMessageDialog(null, "Se registro correctamente!!!", "Éxito", JOptionPane.INFORMATION_MESSAGE);
-
+                    JOptionPane.showMessageDialog(null, "Se registró correctamente!!!", "Éxito", JOptionPane.INFORMATION_MESSAGE);
                 } catch (IOException e) {
-                    System.err.println("Error al escribir en el archivo.");
-                    e.printStackTrace();
+                    System.err.println("Error al escribir en el archivo: " + e.getMessage());
+                } finally {
+                    if (writer != null) {
+                        try {
+                            writer.close();
+                        } catch (IOException e) {
+                            System.err.println("Error al cerrar el archivo: " + e.getMessage());
+                        }
+                    }
                 }
 
             }else{
@@ -84,15 +103,130 @@ public class Controlador {
             JOptionPane.showMessageDialog(null, "Rellene el campo gmail", "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
-    public void agregarProducto(Producto producto) {
+
+    //-------------------------------------------- metodos para el carrito del usuario
+    public void agregarProductoAlCarrito(Producto producto) {
         carrito.agregarItem(producto);
     }
-    public void eleminarProducto(Producto producto) {
+    public void eleminarProductoCarrito(Producto producto) {
         carrito.eliminarItem(producto);
     }
     public void borrarCarrito(){
         carrito.borrarCarrito();
     }
 
+    // ------------------------------------------
+    public void DarAltaProducto(Producto producto){
+        catalogo.Agregar(producto);
+    }
+    public void DarBajaProducto(int codigo){
+        catalogo.Eliminar(codigo);
+    }
 
+    public void modoficarProducto(int codigo,String nombre,String descripcion,float precio){
+        catalogo.Modificar(codigo,nombre,descripcion,precio);
+    }
+
+    //-------------------------------------------
+    // Cargar archivos
+
+    public void cargarArchivos(){
+        String nombreArchivoClientes = "clientes.txt";
+        String rutaCarpeta = "archivos/";
+        String rutaArchivoClientes = rutaCarpeta + nombreArchivoClientes;
+
+        File archivoClientes = new File(rutaArchivoClientes);
+        if (archivoClientes.exists()) {
+            BufferedReader br = null;
+            try {
+                br = new BufferedReader(new FileReader(archivoClientes));
+                String linea;
+                while ((linea = br.readLine()) != null) {
+                    if(!linea.equals("")){
+                        // Separar los datos de la línea y crear un cliente
+                        String[] datosCliente = linea.split(",");
+                        String tipo = datosCliente[0];
+                        String nombreCompleto = datosCliente[1];
+                        String gmail = datosCliente[2];
+                        String contrasena = datosCliente[3];
+                        if (Objects.equals(tipo, "Cliente")) {
+                            Cliente cliente = new Cliente(nombreCompleto, gmail, contrasena);
+                            clientes.add(cliente);
+                        }
+                        if(Objects.equals(tipo, "Administrador")){
+                            Administrador administrador = new Administrador(nombreCompleto, gmail, contrasena);
+                            administradores.add(administrador);
+                        }
+                    }
+                }
+                System.out.println("Clientes cargados desde el archivo correctamente.");
+            } catch (IOException e) {
+                System.err.println("Error al cargar clientes desde el archivo: " + e.getMessage());
+            } finally {
+                if (br != null) {
+                    try {
+                        br.close();
+                    } catch (IOException e) {
+                        System.err.println("Error al cerrar BufferedReader: " + e.getMessage());
+                    }
+                }
+            }
+        }
+
+
+        ArrayList<Producto> ListaProducto = new ArrayList<>();
+        nombreArchivoClientes = "productos.txt";
+        rutaCarpeta = "archivos/";
+        rutaArchivoClientes = rutaCarpeta + nombreArchivoClientes;
+
+        File archivoProducots = new File(rutaArchivoClientes);
+        if (archivoProducots.exists()) {
+            BufferedReader br = null;
+            try {
+                br = new BufferedReader(new FileReader(archivoProducots));
+                String linea;
+                while ((linea = br.readLine()) != null) {
+                    // Separar los datos de la línea y crear un cliente
+                    if(!linea.equals("")){
+                        String[] datosProducto = linea.split(",");
+                        String tipo = datosProducto[0];
+                        int codigo = Integer.parseInt(datosProducto[1]);
+                        String nombre =datosProducto[2];
+                        String descripcion =datosProducto[3];
+                        float precio = Float.parseFloat(datosProducto[4]);
+
+                        if (Objects.equals(tipo, "Electronico")) {
+                            int garantia = Integer.parseInt(datosProducto[5]);
+                            Electronico electronico = new Electronico(codigo,nombre,descripcion,precio,garantia);
+                            ListaProducto.add(electronico);
+
+
+                        }
+                        if (Objects.equals(tipo, "Comida")) {
+                            String fechaDeVencimiento = datosProducto[5];;
+                            float peso = Float.parseFloat(datosProducto[6]);
+                            Comida comida = new Comida(codigo,nombre,descripcion,precio,fechaDeVencimiento,peso);
+                            ListaProducto.add(comida);
+                        }
+                    }
+                }
+                System.out.println("Clientes cargados desde el archivo correctamente.");
+            } catch (IOException e) {
+                System.err.println("Error al cargar clientes desde el archivo: " + e.getMessage());
+            } finally {
+                if (br != null) {
+                    try {
+                        br.close();
+                    } catch (IOException e) {
+                        System.err.println("Error al cerrar BufferedReader: " + e.getMessage());
+                    }
+                }
+            }
+            CatalogoProductos CatalogoActual = new CatalogoProductos(ListaProducto);
+            this.catalogo = CatalogoActual;
+        }
+
+
+
+    }
 }
