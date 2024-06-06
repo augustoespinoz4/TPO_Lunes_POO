f6b0116a08f66c09539d9e7d160691bf444412c5
augustoespinoz4 <129131293+augustoespinoz4@users.noreply.github.com>
2024-06-05 21:38:22 -0300

    Add files via upload

---
+++ TPO_POO_Chascomus_2024/src/App.java
@@ -0,0 +1,15 @@
+import GUI.LoginView;
+
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                LoginView loginView = new LoginView();
+                loginView.setVisible(true);
+            }
+        });
+    }
+}
